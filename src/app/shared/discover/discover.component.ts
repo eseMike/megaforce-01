9a7d3dff7a14a40867eaea27a1f2e8d11a49757c
José Miguel Guerrero Albarrán <mikealbarran@MacBook-Air-de-Jose.local>
2025-10-03 20:07:56 -0600

    update social

--- src/app/shared/discover/discover.component.ts
+++ src/app/shared/discover/discover.component.ts
@@ -1,12 +1,29 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TranslationService } from '../translation.service';
 
 @Component({
   selector: 'app-discover',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './discover.component.html',
   styleUrl: './discover.component.scss'
 })
 export class DiscoverComponent {
   readonly i18n = inject(TranslationService);
+
+  /** Images for the continuous marquee in Discover */
+  carouselImages: string[] = [
+    'assets/images/evento1.jpg',
+    'assets/images/evento2.jpg',
+    'assets/images/evento3.jpg',
+    'assets/images/evento4.jpg',
+    'assets/images/evento5.jpg',
+    'assets/images/evento6.jpg',
+    'assets/images/evento7.jpg',
+    'assets/images/evento8.jpg',
+    'assets/images/evento9.jpg',
+    'assets/images/evento10.jpg',
+    'assets/images/evento11.jpg',
+    'assets/images/evento12.jpg',
+  ];
 }
